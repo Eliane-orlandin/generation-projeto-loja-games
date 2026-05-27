1a308daa9f66786e987421a9903dfc1e21459065
Eliane Orlandin <liorlandin33@gmail.com>
2026-05-27 19:45:02 -0300

    feat: adiciona método POST na classe ProdutoController

--- lojagames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
+++ lojagames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.lojagames.model.Produto;
 import com.generation.lojagames.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -39,4 +43,9 @@ public class ProdutoController {
 		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+		produto.setId(null); 
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+	}
 }
